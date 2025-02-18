2432919398639d2ccc8bb8bdbfaa81a41cc0708a
supercoolfire <bahuiput+github@gmail.com>
2025-02-18 13:59:37 +0800

    Chapter 14; Improving Accessibility

--- nextjs-dashboard/app/ui/dashboard/nav-links.tsx
+++ nextjs-dashboard/app/ui/dashboard/nav-links.tsx
@@ -22,10 +22,11 @@ const links = [
 ];
 
 export default function NavLinks() {
+  const pathname = usePathname();
+
   return (
     <>
       {links.map((link) => {
-        const pathname = usePathname();
         const LinkIcon = link.icon;
         return (
           <Link
